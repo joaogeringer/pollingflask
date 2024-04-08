fc38146cd274790456c680e7cb2452b74df8fd2d
João Geringer <91030587+joaogeringer@users.noreply.github.com>
2024-04-08 16:43:26 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,51 @@
+import os.path
+
+import pandas as pd
+from flask import Flask, render_template, redirect, request, url_for, make_response
+
+app = Flask(__name__, template_folder="Templates")
+
+if not os.path.exists("polls.csv"):
+    structure = {
+        'id': [],
+        'poll': [],
+        "option1": [],
+        "option2": [],
+        "option3": [],
+        'votes1': [],
+        'votes2': [],
+        'votes3': [],
+    }
+
+    pd.DataFrame(structure).set_index('id').to_csv('polls.csv')
+
+polls_df = pd.read_csv('polls.csv').set_index('id')
+
+
+@app.route("/")
+def index():
+    return render_template('index.html', polls=polls_df)
+
+
+@app.route('/polls/<id>')
+def polls(id):
+    poll = polls_df.loc[int(id)]
+    return str(poll)
+
+
+@app.route('/polls', methods=["GET", "POST"])
+def create_polls():
+    if request.method == "GET":
+        pass
+    elif request.method == "POST":
+        pass
+
+
+@app.route('/vote/<id>/<option>')
+def vote(id, option):
+    pass
+
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", debug=True)
